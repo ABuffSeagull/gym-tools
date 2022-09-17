2215f6cbc237dbf16cbd743b4ae0c98c3f5b7629
ABuffSeagull <reecevanatta@hey.com>
2022-09-16 23:02:14 -0400

    Add PWA capabilities

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [elmPlugin()],
+  plugins: [
+    elmPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Tools",
+        short_name: "Tools",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        start_url: "https://workout.abuffseagull.dev",
+        display: "standalone",
+        orientation: "portrait",
+      },
+    }),
+  ],
 });
